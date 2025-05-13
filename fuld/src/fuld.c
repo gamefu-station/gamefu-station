5c5150b9879eedc182d8e4be5592c8cfb6e8d178
Local Atticus <github@nashiora.com>
2025-05-12 21:31:48 -0400

    Get elf parsing corrected

--- fuld/src/fuld.c
+++ fuld/src/fuld.c
@@ -1,9 +1,78 @@
 #define KOS_IMPL
 #include <kos.h>
 
+#define GFU_ELF_IMPL
 #include <gamefu/elf.h>
 
+#include <choir/core.h>
+
+#define INPUT_FILE_MAX 256
+
 int main(int argc, char** argv) {
-    fprintf(stderr, "Hello, FULD!\n");
-    return 0;
+    int result = 0;
+    choir_context_ref context = nullptr;
+
+    const char* output_file = "a.o";
+    const char* input_files[INPUT_FILE_MAX] = {0};
+    gfu_elf elf_data[INPUT_FILE_MAX] = {0};
+    ssize_t input_file_count = 0;
+
+    context = choir_context_create();
+
+    for (int i = 1; i < argc; i++) {
+        const char* arg = argv[i];
+        if (0 == strcmp("-o", arg)) {
+            output_file = argv[++i];
+        } else {
+            if (input_file_count >= INPUT_FILE_MAX) {
+                choir_diag_issue(context, CHOIR_ERROR, "Too many input files. Max is %d.", INPUT_FILE_MAX);
+                kos_return_defer(1);
+            }
+
+            input_files[input_file_count++] = arg;
+        }
+    }
+
+    // TODO(local): check if the output file is part of the input list, that's probably not intended
+
+    for (ssize_t i = 0; i < input_file_count; i++)
+    {
+        FILE* f = fopen(input_files[i], "rb");
+        if (f == nullptr) {
+            choir_diag_issue(context, CHOIR_ERROR, "Failed to open ELF object file '%s'.", input_files[i]);
+            kos_return_defer(1);
+        }
+
+        bool success = gfu_elf_from_file(&elf_data[i], f);
+        fclose(f);
+
+        if (!success) {
+            choir_diag_issue(context, CHOIR_ERROR, "Failed to read ELF object file '%s'.", input_files[i]);
+            kos_return_defer(1);
+        }
+
+        gfu_elf_debug_print(&elf_data[i]);
+
+        //fprintf(stderr, ";; ELF Hexdump for '%s'\n", input_files[i]);
+        //kos_hexdump(elf_data[i].raw.file_data, (ssize_t)elf_data[i].raw.data_size);
+
+        for (uint32_t j = 0; j < elf_data[i].segment_count; j++) {
+            fprintf(stderr, ";; Segment 0x%04X for '%s'\n", elf_data[i].segments[j].type, input_files[i]);
+            kos_hexdump(elf_data[i].segments[j].data, (ssize_t)elf_data[i].segments[j].data_size);
+            fprintf(stderr, "\n");
+        }
+
+        for (uint32_t j = 0; j < elf_data[i].section_count; j++) {
+            fprintf(stderr, ";; Section %s (0x%04X) for '%s'\n", elf_data[i].sections[j].name, elf_data[i].raw.sections[j].name_index, input_files[i]);
+            kos_hexdump(elf_data[i].sections[j].data, (ssize_t)elf_data[i].sections[j].data_size);
+            fprintf(stderr, "\n");
+        }
+    }
+
+defer:;
+    for (ssize_t i = 0; i < input_file_count; i++)
+        gfu_elf_free(&elf_data[i]);
+
+    choir_context_destroy(context);
+    return result;
 }
--- include/gamefu/elf.h
+++ include/gamefu/elf.h
@@ -214,4 +214,268 @@ static_assert(sizeof(gfu_elf_header) == 52);
 static_assert(sizeof(gfu_elf_ph_entry) == 0x20);
 static_assert(sizeof(gfu_elf_sh_entry) == 0x28);
 
+typedef struct gfu_elf_raw {
+    gfu_elf_header header;
+    gfu_elf_ph_entry* segments;
+    gfu_elf_sh_entry* sections;
+    uint32_t data_size;
+    char* file_data;
+} gfu_elf_raw;
+
+typedef struct gfu_elf_segment {
+    uint32_t offset;
+
+    uint32_t type;
+    uint32_t virtual_address;
+    uint32_t physical_address;
+    uint32_t memory_size;
+    uint32_t flags;
+    uint32_t align;
+
+    uint32_t data_size;
+    char* data;
+} gfu_elf_segment;
+
+typedef struct gfu_elf_section {
+    uint32_t offset;
+
+    const char* name;
+    uint32_t type;
+    uint32_t flags;
+    uint32_t virtual_address;
+    uint32_t link;
+    uint32_t info;
+    uint32_t align;
+
+    uint32_t data_size;
+    char* data;
+} gfu_elf_section;
+
+typedef struct gfu_elf {
+    gfu_elf_raw raw;
+
+    uint8_t endianness;
+    uint32_t flags;
+    uint32_t entry;
+
+    uint32_t segment_count;
+    gfu_elf_segment* segments;
+
+    uint32_t section_count;
+    gfu_elf_section* sections;
+} gfu_elf;
+
+bool gfu_elf_from_file(gfu_elf* elf, FILE* f);
+bool gfu_elf_from_bytes(gfu_elf* elf, char* data, size_t size);
+void gfu_elf_free(gfu_elf* elf);
+void gfu_elf_debug_print(gfu_elf* elf);
+
 #endif /* GAMEFU_ELF_H_ */
+
+
+#if defined(GFU_ELF_IMPL) || defined(GFU_ELF_IMPLEMENTATION)
+#undef GFU_ELF_IMPL
+#undef GFU_ELF_IMPLEMENTATION
+
+bool gfu_elf_from_file(gfu_elf* elf, FILE* f) {
+    if (0 != fseek(f, 0, SEEK_END)) {
+        return false;
+    }
+
+    int64_t file_size = ftell(f);
+    if (file_size < 0) {
+        return false;
+    }
+
+    if (0 != fseek(f, 0, SEEK_SET)) {
+        return false;
+    }
+
+    char* data = calloc(1, (size_t)file_size);
+    size_t read_count = fread(data, (size_t)file_size, 1, f);
+
+    if (ferror(f)) {
+        return false;
+    }
+
+    return gfu_elf_from_bytes(elf, data, (size_t)file_size);
+}
+
+bool gfu_elf_from_bytes(gfu_elf* elf, char* data, size_t size) {
+    char* data_start = data;
+    const char* data_end = data + size;
+
+    // TODO(local): define actual implementation for ELFTOHOST2 and ELFTOHOST4
+#define ELFTOHOST2(Value) (Value)
+#define ELFTOHOST4(Value) (Value)
+#define READ1() (assert(data < data_end), data += 1, *(data - 1))
+#define READ2() (assert(data < data_end), data += 2, ELFTOHOST2(*(uint16_t*)(data - 2)))
+#define READ4() (assert(data < data_end), data += 4, ELFTOHOST4(*(uint32_t*)(data - 4)))
+
+    *elf = (gfu_elf){0};
+    elf->raw.data_size = (uint32_t)size;
+    elf->raw.file_data = data;
+
+    gfu_elf_header* header = &elf->raw.header;
+    header->magic[0] = READ1();
+    header->magic[1] = READ1();
+    header->magic[2] = READ1();
+    header->magic[3] = READ1();
+    header->class = READ1();
+    header->endianness = READ1();
+    header->version = READ1();
+    header->abi = READ1();
+    header->abi_version = READ1();
+    data += 7; // padding
+    header->type = READ2();
+    assert(header->type == GFU_ELF_TYPE_REL);
+    header->machine = READ2();
+    header->version2 = READ4();
+    header->entry = READ4();
+    header->ph_offset = READ4();
+    header->sh_offset = READ4();
+    header->flags = READ4();
+    header->header_size = READ2();
+    assert(header->header_size == sizeof(gfu_elf_header));
+    header->ph_entry_size = READ2();
+    assert(header->ph_entry_size == 0 || header->ph_entry_size == sizeof(gfu_elf_ph_entry));
+    header->ph_count = READ2();
+    header->sh_entry_size = READ2();
+    assert(header->sh_entry_size == 0 || header->sh_entry_size == sizeof(gfu_elf_sh_entry));
+    header->sh_count = READ2();
+    header->sh_names_index = READ2();
+
+    elf->raw.segments = calloc((size_t)header->ph_count, sizeof *elf->raw.segments);
+    elf->raw.sections = calloc((size_t)header->sh_count, sizeof *elf->raw.sections);
+
+    for (uint32_t i = 0; i < header->ph_count; i++) {
+        data = data_start + header->ph_offset + (i * header->ph_entry_size);
+        gfu_elf_ph_entry* entry = &elf->raw.segments[i];
+        entry->type = READ4();
+        entry->offset = READ4();
+        entry->virtual_address = READ4();
+        entry->physical_address = READ4();
+        entry->file_size = READ4();
+        entry->memory_size = READ4();
+        entry->flags = READ4();
+        entry->align = READ4();
+    }
+
+    for (uint32_t i = 0; i < header->sh_count; i++) {
+        data = data_start + header->sh_offset + (i * header->sh_entry_size);
+        gfu_elf_sh_entry* entry = &elf->raw.sections[i];
+        entry->name_index = READ4();
+        entry->type = READ4();
+        entry->flags = READ4();
+        entry->virtual_address = READ4();
+        entry->offset = READ4();
+        entry->size = READ4();
+        entry->link = READ4();
+        entry->info = READ4();
+        entry->align = READ4();
+        entry->entry_size = READ4();
+    }
+
+    // transform the raw
+
+    elf->endianness = header->endianness;
+    elf->flags = header->flags;
+    elf->entry = header->entry;
+    elf->segment_count = header->ph_count;
+    elf->segments = calloc((size_t)elf->segment_count, sizeof *elf->segments);
+    elf->section_count = header->sh_count;
+    elf->sections = calloc((size_t)elf->section_count, sizeof *elf->sections);
+
+    const char* name_table;
+    {
+        gfu_elf_sh_entry* name_entry = &elf->raw.sections[elf->raw.header.sh_names_index];
+        name_table = data_start + name_entry->offset;
+    }
+
+    for (uint32_t i = 0; i < elf->segment_count; i++) {
+        gfu_elf_ph_entry* entry = &elf->raw.segments[i];
+        gfu_elf_segment* segment = &elf->segments[i];
+
+        segment->offset = entry->offset;
+
+        segment->data_size = entry->file_size;
+        segment->data = data_start + entry->offset;
+    }
+
+    for (uint32_t i = 0; i < elf->section_count; i++) {
+        gfu_elf_sh_entry* entry = &elf->raw.sections[i];
+        gfu_elf_section* section = &elf->sections[i];
+
+        section->name = &name_table[entry->name_index];
+
+        section->data_size = entry->size;
+        section->data = data_start + entry->offset;
+    }
+
+#undef READ4
+#undef READ2
+#undef READ1
+#undef ELFTOHOST4
+#undef ELFTOHOST2
+
+    return true;
+}
+
+void gfu_elf_free(gfu_elf* elf) {
+    free(elf->raw.file_data);
+    free(elf->raw.segments);
+    free(elf->raw.sections);
+    free(elf->segments);
+    free(elf->sections);
+}
+
+void gfu_elf_debug_print(gfu_elf* elf) {
+    fprintf(stderr, "ELF Header:\n");
+
+    fprintf(stderr, "  Magic:  ");
+    for (int i = 0; i < 4; i++) {
+        fprintf(stderr, " %02X", elf->raw.header.magic[i]);
+    }
+    fprintf(stderr, "\n");
+
+    fprintf(stderr, "Class:                             %u\n", elf->raw.header.class);
+    fprintf(stderr, "Data:                              %u\n", elf->raw.header.endianness);
+    fprintf(stderr, "Version:                           %u\n", elf->raw.header.version);
+    fprintf(stderr, "OS/ABI:                            %u\n", elf->raw.header.abi);
+    fprintf(stderr, "ABI Version:                       %u\n", elf->raw.header.abi_version);
+    fprintf(stderr, "Type:                              %u\n", elf->raw.header.type);
+    fprintf(stderr, "Machine:                           %u\n", elf->raw.header.machine);
+    fprintf(stderr, "Version:                           0x%02X\n", elf->raw.header.version2);
+    fprintf(stderr, "Entry point address:               0x%04X\n", elf->raw.header.entry);
+    fprintf(stderr, "Start of program headers:          0x%08X (%u bytes into file)\n", elf->raw.header.ph_offset, elf->raw.header.ph_offset);
+    fprintf(stderr, "Start of section headers:          0x%08X (%u bytes into file)\n", elf->raw.header.sh_offset, elf->raw.header.sh_offset);
+    fprintf(stderr, "Flags:                             0x%08X\n", elf->raw.header.flags);
+    fprintf(stderr, "Size of this header:               %u (bytes)\n", elf->raw.header.header_size);
+    fprintf(stderr, "Size of program headers:           %u (bytes)\n", elf->raw.header.ph_entry_size);
+    fprintf(stderr, "Number of program headers:         %u\n", elf->raw.header.ph_count);
+    fprintf(stderr, "Size of section headers:           %u (bytes)\n", elf->raw.header.sh_entry_size);
+    fprintf(stderr, "Number of section headers:         %u\n", elf->raw.header.sh_count);
+    fprintf(stderr, "Section header string table index: %u\n", elf->raw.header.sh_names_index);
+
+    fprintf(stderr, "\nSegment Headers:\n");
+
+    fprintf(stderr, "\nSection Headers:\n");
+
+    fprintf(stderr, "  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
+    const char* name_table = elf->sections[elf->raw.header.sh_names_index].data;
+    for (uint32_t i = 0; i < elf->section_count; i++) {
+        int nleft;
+        fprintf(stderr, "  [%2u] ", i);
+        nleft = fprintf(stderr, "%s", &name_table[elf->raw.sections[i].name_index]);
+        for (int j = 0; j < 18 - nleft; j++) fputc(' ', stderr);
+        nleft = fprintf(stderr, "%02X", elf->raw.sections[i].type);
+        for (int j = 0; j < 16 - nleft; j++) fputc(' ', stderr);
+        fprintf(stderr, "%08X ", elf->raw.sections[i].virtual_address);
+        fprintf(stderr, "%06X ", elf->raw.sections[i].offset);
+        fprintf(stderr, "%06X ", elf->raw.sections[i].size);
+        fprintf(stderr, "%02X ", elf->raw.sections[i].entry_size);
+        fprintf(stderr, "\n");
+    }
+}
+
+#endif /* GFU_ELF_IMPL */
