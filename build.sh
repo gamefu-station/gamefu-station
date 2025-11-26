4579237a855ca7237fafc7c495c85c7dd9c2d01c
Emily Choe <echoephile+codeberg@gmail.com>
2025-11-26 10:57:24 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -24,29 +24,28 @@ cc -o bin/as gfu-as/as.c @${flags}.txt
 # echo "Building GameFU C Compiler..."
 # cc -o fucc src/fucc.c @${flags}.txt
 
-# echo "Building GameFU Compiler Collection..."
-# cc -o fucc src/fucc2.c @${flags}.txt
-
 echo "Building GameFU Station BIOS..."
-./bin/as -o bios.gfu lib/bios/boot.fus
-./bin/hx bios.gfu -i -n gfusx_default_bios > ./gfu-sx/default_bios.h
-# rm bios.gfu # once it's in the include file we don't need the raw binary, de-clutter while we're here
+./bin/as -o bin/bios.gfu lib/bios/boot.fus
+./bin/hx bin/bios.gfu -i -n gfusx_default_bios > ./gfu-sx/default_bios.h
+rm bin/bios.gfu # once it's in the include file we don't need the raw binary, de-clutter while we're here
 
 gfusx_platform_lib=""
-if [ "$gfusx_platform" = "GLFW" ] && [ ! -f bin/libglfw3.a ]; then
-    echo "Building GLFW3..."
-    cc -o glfw3.o -c lib/glfw3/glfw3.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    cc -o glfw3_linux.o -c lib/glfw3/glfw3_linux.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    cc -o glfw3_null.o -c lib/glfw3/glfw3_null.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    cc -o glfw3_osmesa.o -c lib/glfw3/glfw3_osmesa.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    cc -o glfw3_wl.o -c lib/glfw3/glfw3_wl.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    cc -o glfw3_x11.o -c lib/glfw3/glfw3_x11.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    ar rcs bin/libglfw3.a glfw3.o glfw3_linux.o glfw3_null.o glfw3_osmesa.o glfw3_wl.o glfw3_x11.o
-    rm glfw3.o glfw3_linux.o glfw3_null.o glfw3_osmesa.o glfw3_wl.o glfw3_x11.o
+if [ "$gfusx_platform" = "GLFW" ]; then
     gfusx_platform_lib=bin/libglfw3.a
+    if [ ! -f bin/libglfw3.a ]; then
+        echo "Building GLFW3..."
+        cc -o bin/glfw3.o -c lib/glfw3/glfw3.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        cc -o bin/glfw3_linux.o -c lib/glfw3/glfw3_linux.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        cc -o bin/glfw3_null.o -c lib/glfw3/glfw3_null.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        cc -o bin/glfw3_osmesa.o -c lib/glfw3/glfw3_osmesa.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        cc -o bin/glfw3_wl.o -c lib/glfw3/glfw3_wl.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        cc -o bin/glfw3_x11.o -c lib/glfw3/glfw3_x11.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
+        ar rcs bin/libglfw3.a bin/glfw3.o bin/glfw3_linux.o bin/glfw3_null.o bin/glfw3_osmesa.o bin/glfw3_wl.o bin/glfw3_x11.o
+        rm bin/glfw3.o bin/glfw3_linux.o bin/glfw3_null.o bin/glfw3_osmesa.o bin/glfw3_wl.o bin/glfw3_x11.o
+    fi
 else
-    gfusx_platform_lib="NULL"
+    gfusx_platform_lib=""
 fi
 
 echo "Building GFUSX Emulator..."
-cc -o bin/sx gfu-sx/sx.c @${flags}.txt -DGFUSX_PLATFORM_$gfusx_platform -Ivendor/glfw3/include bin/libglfw3.a -lm
+cc -o bin/sx gfu-sx/sx.c @${flags}.txt -DGFUSX_PLATFORM_$gfusx_platform -Ivendor/glfw3/include ${gfusx_platform_lib} -lm
