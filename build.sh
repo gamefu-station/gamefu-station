87774c1d3abf867cb981345aab2b41a894ce6c3c
Emily Choe <echoephile+codeberg@gmail.com>
2025-11-26 10:46:34 -0500

    Move fuasm.isel

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo "Building GameFU HX Utility..."
 cc -o bin/hx gfu-hx/hx.c @${flags}.txt
 
 echo "Building GameFU Assembler ISel Table Generator..."
-./bin/hx ./lib/fuasm.isel -i -n isel > ./iselgen/isel_source.h
+./bin/hx ./iselgen/isel.txt -i -n isel > ./iselgen/isel_source.h
 cc -o bin/iselgen iselgen/iselgen.c @${flags}.txt
 
 echo "Running GameFU Assembler ISel Table Generator..."
@@ -33,7 +33,7 @@ echo "Building GameFU Station BIOS..."
 # rm bios.gfu # once it's in the include file we don't need the raw binary, de-clutter while we're here
 
 gfusx_platform_lib=""
-if [ "$gfusx_platform" = "GLFW" ] && [ ! -f libglfw3.a ]; then
+if [ "$gfusx_platform" = "GLFW" ] && [ ! -f bin/libglfw3.a ]; then
     echo "Building GLFW3..."
     cc -o glfw3.o -c lib/glfw3/glfw3.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
     cc -o glfw3_linux.o -c lib/glfw3/glfw3_linux.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
@@ -41,12 +41,12 @@ if [ "$gfusx_platform" = "GLFW" ] && [ ! -f libglfw3.a ]; then
     cc -o glfw3_osmesa.o -c lib/glfw3/glfw3_osmesa.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
     cc -o glfw3_wl.o -c lib/glfw3/glfw3_wl.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
     cc -o glfw3_x11.o -c lib/glfw3/glfw3_x11.c -Wno-macro-redefined -D_GNU_SOURCE -Ivendor/glfw3/include
-    ar rcs libglfw3.a glfw3.o glfw3_linux.o glfw3_null.o glfw3_osmesa.o glfw3_wl.o glfw3_x11.o
+    ar rcs bin/libglfw3.a glfw3.o glfw3_linux.o glfw3_null.o glfw3_osmesa.o glfw3_wl.o glfw3_x11.o
     rm glfw3.o glfw3_linux.o glfw3_null.o glfw3_osmesa.o glfw3_wl.o glfw3_x11.o
-    gfusx_platform_lib=libglfw3.a
+    gfusx_platform_lib=bin/libglfw3.a
 else
     gfusx_platform_lib="NULL"
 fi
 
 echo "Building GFUSX Emulator..."
-cc -o gfusx src/gfusx.c @${flags}.txt -DGFUSX_PLATFORM_$gfusx_platform -Ivendor/glfw3/include libglfw3.a -lm
+cc -o gfusx src/gfusx.c @${flags}.txt -DGFUSX_PLATFORM_$gfusx_platform -Ivendor/glfw3/include bin/libglfw3.a -lm
